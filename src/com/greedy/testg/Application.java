85f6d631b74ac8fd5e48722ef22d7d2499834166
tj <tj@DESKTOP-MOB8F55>
2021-01-18 12:10:36 +0900

    두번째가 스페이스바 추가

--- src/com/greedy/testg/Application.java
+++ src/com/greedy/testg/Application.java
@@ -10,8 +10,7 @@ public class Application {
 		
 		System.out.println("나는 master야");
 		System.out.println("잘 만들어서 올렸네 나도 잘 받았어!");
-		System.out.println("아니야!! 내가 master야!!!!!!!!!!!!!");
-	
+		System.out.println("아니야!! 내가 master야!!!!!!!!!!!!!"); 
 	}
 	
 }
